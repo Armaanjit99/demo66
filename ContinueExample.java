07ffd21109db1a2873bbef8b2864abc50beef270
Armaanjit99 <101565344+Armaanjit99@users.noreply.github.com>
2022-03-28 15:52:49 +0530

    Add files via upload

---
+++ ContinueExample.java
@@ -0,0 +1,16 @@
+package Basiccommands;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		for(int i= 1;i<=20;i++)
+		{	
+			if(i==15)
+			continue;
+			System.out.print("  " + i);
+			
+		}
+
+	}
+
+}
